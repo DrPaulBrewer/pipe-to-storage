59f913ea98dff04a27c20607083aa23039d44595
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2017-03-28 01:38:56 +0000

    use storage-friendly retry rules, accepting string or function ()=>stream as "rewindable" source;  keep one-attempt functionality on standard readstream

--- index.js
+++ index.js
@@ -3,9 +3,45 @@
 
 /* jshint esnext:true,eqeqeq:true,undef:true,lastsemic:true,strict:true,unused:true,node:true */
 
-module.exports = function pipeToStorage(storage){
+const intoStream = require('into-stream');
+const promiseRetry = require('promise-retry');
+
+function storeOrFail(storage, localStream, bucketName, fileName, wsOptions){
+    return new Promise(function(resolve, reject){
+	const remote = (storage
+			.bucket(bucketName)
+			.file(fileName)
+			.createWriteStream(wsOptions)
+		       );
+
+	// writing is finished when .finish is fired on remote
+	// https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/0.8.0/storage/file?method=createWriteStream
+	
+	remote.on('finish', function(){
+	    resolve({bucket: bucketName, file: fileName});
+	});
+	localStream.on('error', function(e){
+	    remote.end();
+	    reject("piptToStorage: error reading local input stream:"+e);
+	});
+	remote.on('error', function(e){
+	    reject("pipeToStorage: error while writing gs://"+bucketName+"/"+fileName+":"+e);
+	});
+	localStream.pipe(remote);
+    });
+};
+
+
+module.exports = function pipeToStorage(storage, _retryStrategy){
     "use strict";
-    return function(localStream, bucketName, fileName, opt){
+    const retryStrategy = _retryStrategy || {
+	retries: 10,
+	factor: 1.5,
+	minTimeout:  1000,
+	maxTimeout: 10000,
+	randomize: true
+    };
+    return function(source, bucketName, fileName, opt){
 	function contentType(what){
 	    return {
 		metadata: {
@@ -15,6 +51,12 @@ module.exports = function pipeToStorage(storage){
 	}
 	let meta;
 	let wsOptions = {resumable:false};
+	let streamer;
+	if (typeof(source)==='string'){
+	    streamer = ()=>(intoStream(source));
+	} else if (typeof(source)==='function'){
+	    streamer = source;
+	}		
 	if (opt === 'json'){
 	    meta = contentType('application/json');
 	} else if (opt && (typeof(opt)==='string')){
@@ -24,28 +66,14 @@ module.exports = function pipeToStorage(storage){
 	}
 	if (meta)
 	    Object.assign(wsOptions, meta);
-	return new Promise(function(resolve, reject){
-	    const remote = (storage
-			    .bucket(bucketName)
-			    .file(fileName)
-			    .createWriteStream(wsOptions)
-			   );
-
-	    // writing is finished when .finish is fired on remote
-	    // https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/0.8.0/storage/file?method=createWriteStream
-	    
-	    remote.on('finish', function(){
-		resolve({bucket: bucketName, file: fileName});
-	    });
-	    localStream.on('error', function(e){
-		remote.end();
-		reject("piptToStorage: error reading local input stream:"+e);
-	    });
-	    remote.on('error', function(e){
-		reject("pipeToStorage: error while writing gs://"+bucketName+"/"+fileName+":"+e);
-	    });
-	    localStream.pipe(remote);
-	});
-    };
+	if (typeof(streamer)==='function'){
+	    return promiseRetry(function(retry, attempt){
+		const localStream = streamer();
+		return storeOrFail(storage, localStream, bucketName, fileName, wsOptions).catch(function(e){
+		    console.log("error on attempt: ", attempt);
+		    console.log(e);
+		});
+	    }, retryStrategy);
+	}
+	return storeOrFail(storage, source, bucketName, fileName, wsOptions);
 };
-    
